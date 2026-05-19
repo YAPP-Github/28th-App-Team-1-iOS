aedd3a92d3e687e03c3da22e283505e2eca88341
alice9547 <alice9547@naver.com>
2026-05-19 12:24:54 +0900

    Initial Commit

---
+++ Architecture/ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureApp.swift
+//  Architecture
+//
+//  Created by EunseoKim on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
