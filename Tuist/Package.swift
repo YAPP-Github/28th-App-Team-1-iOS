71cc4fc0fe85acd03dfe9470661a0202762a8d36
서정원 <indian4589@naver.com>
2026-07-09 13:36:55 +0900

    add: Kakao iOS SDK SPM 의존성 추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 
     let packageSettings = PackageSettings(
         productTypes: [
-            "ComposableArchitecture": .framework
+            "ComposableArchitecture": .framework,
+            "KakaoSDKCommon": .framework,
+            "KakaoSDKAuth": .framework,
+            "KakaoSDKUser": .framework
         ],
         // 외부 SPM 의존도 워크스페이스와 동일한 3단계 Configuration 으로 생성한다.
         // (없으면 Debug/Release 로 생성돼 Dev/QA 빌드에서 구성 불일치 경고·폴백이 난다)
@@ -23,6 +26,7 @@ import PackageDescription
 let package = Package(
     name: "App",
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.25.0")
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.25.0"),
+        .package(url: "https://github.com/kakao/kakao-ios-sdk", from: "2.22.0")
     ]
 )
