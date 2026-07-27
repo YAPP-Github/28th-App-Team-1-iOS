5abffcebce369968cf35b5c1baa81a25e74b6fab
서정원 <indian4589@naver.com>
2026-07-27 15:38:24 +0900

    feat: DomainPermission 모듈 신설_카메라 마이크 권한 Client

---
+++ Projects/Domain/DomainPermission/Tests/DomainPermissionTests.swift
@@ -0,0 +1,11 @@
+//
+//  DomainPermissionTests.swift
+//  DomainPermissionTests
+//
+//  Created by 서정원 on 26/07/27.
+//
+
+import XCTest
+@testable import DomainPermissionImplementation
+
+// 단위테스트는 작성하지 않는다. placeholder.
